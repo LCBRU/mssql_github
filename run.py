ac6100e9f565923f1b992484377e9b85b0c74cb7
Richard Bramley <rabramley@gmail.com>
2019-11-25 14:51:47 +0000

    "25/11/2019"

--- run.py
+++ run.py
@@ -1,26 +1,95 @@
 import schedule
+import shutil
 import time
 import logging
 import os
+import subprocess as sp
 from dotenv import load_dotenv
 import json
+import argparse
+from datetime import date
 
-def job():
+
+def run():
     databases = json.loads(os.getenv('DATABASES', "[]"))
+    server = os.environ['MSSQL_SERVER']
+    user = os.environ['MSSQL_USER']
+    password = os.environ['MSSQL_PASSWORD']
+    repository_dir = os.environ['REPOSITORY_DIR']
+    backup_subdir = os.environ['BACKUP_SUBDIR']
 
     for d in databases:
-        print(d)
+        db_backup_dir = os.path.join(repository_dir, backup_subdir, d)
+        print(f" Scripting {d} to {db_backup_dir}")
+        
+        shutil.rmtree(db_backup_dir)
+        sp.run([
+            'mssql-scripter',
+            '--server', server,
+            '--user', user,
+            '--password', password,
+            '-d', d,
+            '--file-path', db_backup_dir,
+            '--file-per-object',
+            '--exclude-headers',
+            '--exclude-use-database',
+        ])
 
-    # os.system('mssql-scripter --server UHLSQLBRICCSDB\\\\UHLBRICCSDB --user briccs_admin --password bR1cc5100 -d i2b2_app03_ASProgression_Data --file-path /home/richard/projects/mssql_github/backup/ --file-per-object --exclude-headers --exclude-use-database')
+        sp.run([
+            'git',
+            'add',
+            '-A',
+        ])
 
-load_dotenv()
+        sp.run([
+            'git',
+            'commit',
+            '-m',
+            f'"{date.today():%d/%m/%Y}"',
+        ])
 
-schedule.every(1).minutes.do(job)
+        sp.run([
+            'git',
+            'push',
+        ])
 
-try:
-    while True:
+
+def get_parameters():
+    parser = argparse.ArgumentParser(description='Script MS SQL databases and check them into Github')
+    parser.add_argument(
+        "-r",
+        "--run",
+        help="Run scripting",
+        action="store_true",
+    )
+
+    args = parser.parse_args()
+
+    return args
+
+
+def schedule_scripting():
+    schedule.every(1).minutes.do(run)
+
+    try:
+        while True:
             schedule.run_pending()
             time.sleep(1)
 
-except KeyboardInterrupt:
-    logging.info('Schedule stopped')
+    except KeyboardInterrupt:
+        logging.info('Schedule stopped')
+
+
+load_dotenv()
+
+logging.basicConfig(
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+    level=logging.INFO
+)
+
+args = get_parameters()
+
+if args.run:
+    run()
+else:
+    schedule_scripting()
